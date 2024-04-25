7f331a2329e59f5ecaf73b275d897a7cda96db22
VICTOR LEO <156098396+VICLEO21@users.noreply.github.com>
2024-04-24 23:31:14 -0600

    f

--- main.py
+++ main.py
@@ -1,67 +1,45 @@
 import pandas as pd
 import openpyxl
 from lib import *
+import threading
+import time
+import logging
+from concurrent.futures import ThreadPoolExecutor
 
+logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 
+#-----------------------------------------------------------------------------
 
-df = pd.read_excel(nombre_archivo,sheet_name='Hoja1',engine='openpyxl')
+nombre_archivo = input("Nombre del archivo: ")
+
+df = pd.read_excel(nombre_archivo,sheet_name='Sheet',engine='openpyxl')
 array = df.values
 
-print("")
-print("Matriz de Adyacencia: ")
-print (array)
-print("")
 
-vertices = df.columns.tolist()
-aristas = []
+globalArrayNum = []
+def contadorDos(inicio,fin):
+    logging.info(f'Funcion con rango: {inicio} - {fin}')
+    for i in range (inicio,fin+1,1):
+        globalArrayNum.append(i)
+        time.sleep(0.01)
+    return 0
 
-for i in range(df.shape[0]):
-    origen = df.index[i]
-    for j in range(df.shape[1]):
-        destino = df.columns[j]
-        peso = df.iloc[i, j]
-        if pd.notna(peso) and peso != 0:
-            aristas.append((origen, destino, peso))
+subrango = 200//4
+rangoActual = 1
+executors = 4
 
+with ThreadPoolExecutor(max_workers=2) as executor:
+    for i in range (1,executors +1,1):
+        j = subrango * i
+        executor.submit(contadorDos, rangoActual , j)
+        rangoActual = subrango + rangoActual
+
+
+sumatoria = 0
+for fila in array:
+    for elemento in fila:
+        sumatoria += elemento
             
-cadena = ""
-for arista in aristas:
-    cadena += f"[{arista[0]}] -> [{arista[1]}] : Peso [{arista[2]}], "
-
-print("Lista de relación: ")
-print(cadena[:-2]) 
-print ("")
-
-df = pd.read_excel(nombre_archivo, index_col=0,)
-    # Hacer diccionarios
-arr = df.to_dict(orient='index')
-arr = {str(k): v for k, v in arr.items()}
-
-
-
-nodos = list(arr.keys())
-
-
-aristas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-
-# etiquetas de los vértices
-vertices = dict(zip(nodos, aristas))
-
-inicio = 'B'
-fin = 'H'
-caminoCorto, paths, nodos_Visitados = dijkstra(arr, inicio, nodos)
-print("")
-print("Path: ")
-print("")
-
-print("Cola: ")
-print("")
-
-print("Visitados:", "','".join([vertices[node] for node in nodos_Visitados]))
-print("")
-
-print("Recorrido:", "','".join(paths[fin]))
-print("")
-
-print(f"Peso Total: {caminoCorto[fin]}")
-print("")
+            
+print (sumatoria)            
+    
